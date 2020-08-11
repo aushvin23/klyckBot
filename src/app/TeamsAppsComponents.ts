b589f34d441b1117597c2b425014bd3adab201b9
aushvin vasanthakumaran <aushvin.vasanth@klyck.io>
2020-08-11 11:43:50 -0400

    Initial Commit

---
+++ src/app/TeamsAppsComponents.ts
@@ -0,0 +1,6 @@
+// Components will be added here
+export const nonce = {}; // Do not remove!
+// Automatically added for the yoTeamsTab tab
+export * from "./yoTeamsTab/YoTeamsTab";
+// Automatically added for the yoTeamsBasicBot bot
+export * from "./yoTeamsBasicBot/YoTeamsBasicBot";
---
+++ src/app/scripts/client.ts
@@ -0,0 +1,6 @@
+// Automatically added for the yoTeamsTab tab
+export * from "./yoTeamsTab/YoTeamsTab";
+export * from "./yoTeamsTab/YoTeamsTabConfig";
+export * from "./yoTeamsTab/YoTeamsTabRemove";
+// Automatically added for the yoTeamsBasicBot bot tab
+export * from "./yoTeamsBasicBot/YoTeamsBasicBotTab";
